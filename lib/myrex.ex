17259a39cdf3df4d1d42fbb88fa94046ec7f998e
HaikerMaiker <a00830026@tec.mx>
2022-04-22 00:14:02 -0500

    Funciones para hacer el html

--- lib/myrex.ex
+++ lib/myrex.ex
@@ -11,4 +11,28 @@ def format(tokens) do
   end)
 end
 
+def imprimir(lista, _contador, _file) when lista == [] do
+  "Terminado"
+end
+
+def imprimir(lista, contador, file) do
+  cont = contador
+  if cont == elem(hd(lista), 0) do
+    IO.binwrite(file, elem(hd(lista), 1))
+    imprimir(tl(lista), cont, file)
+  else
+    cont = cont + 1
+    IO.binwrite(file, "<br>")
+    imprimir(lista, cont, file)
+  end
+end
+
+def analizar(filename, outputname) do
+  procesado = process(filename)
+  formateado = format(elem(procesado, 1))
+  {:ok, archivo} = File.open(outputname, [:write])
+  IO.binwrite(archivo, '<link rel = "stylesheet" href="coloreado.css">')
+  imprimir(formateado, 1, archivo)
+
+end
 end
